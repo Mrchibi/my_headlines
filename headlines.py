1c28866726c1490d6075d444f9e8afd9c9ad3ab4
Jianyu <bb1034084869@163.com>
2018-01-13 19:46:25 +0800

    支持用户输入的方式获取headlines
    支持用户输入的方式获取headlines

--- headlines.py
+++ headlines.py
@@ -2,7 +2,7 @@
 # -*- coding:UTF-8 -*-
 from __future__ import  unicode_literals
 import feedparser
-from flask import  Flask,render_template
+from flask import  Flask,render_template,request
 
 app = Flask(__name__)#创建程序实例，该实例为flask类的对象，
 #wsgi协议把接收到的请求转交给这个对象处理
@@ -16,13 +16,17 @@ RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
 
 
 
-@app.route('/')#执行与/<publication>一样的视图函数
-@app.route('/<publication>')#使用动态路由获取不同网站的headlines
+@app.route('/')#执行的视图函数
+
 
 def get_news(publication="zhihu"):
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = "songshuhui"
+    else:
+        publication =query.lower()
+    
     feed = feedparser.parse(RSS_FEED[publication])#parse函数解析rss
-    
-    
     
     return render_template('home.html',articles=feed['entries'])#feed['entries']表示一组文章的条目
     
